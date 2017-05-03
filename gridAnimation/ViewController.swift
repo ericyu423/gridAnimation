04234490ca8bb0ea6e8236042f6e9846acd03579
eric yu <ericyu423@erics-macbook-pro.local>
2017-05-03 08:27:49 -0700

    working, still need work in auto constraints

--- gridAnimation/ViewController.swift
+++ gridAnimation/ViewController.swift
@@ -9,17 +9,146 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var cells = [String: UIView]()//key,view
+    var width: CGFloat {
+        get {
+            
+            return view.frame.width * 0.05
+        }
+        set
+        {
+            
+            displayBlocks()
+        }
+    }
+    
+    var numViewPerRow: Int {
+        return Int(view.frame.width / width)
+        
+    }
+    
+    var numViewPerColum: Int {
+        
+        return Int(view.frame.height / width)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        displayBlocks()
+        
+    }
+    
+    func displayBlocks(){
+        
+        if UIDevice.current.orientation.isLandscape {
+            
+            for j in 0...numViewPerRow{
+                for i in 0...numViewPerColum{
+                    let squareV = UIView()
+                    squareV.backgroundColor = randomColor()
+                    //squareV.frame = CGRect(x: CGFloat(i) * width, y:
+                    //  CGFloat(j) * width, width: width, height: width)
+                    squareV.layer.borderColor = UIColor.black.cgColor
+                    squareV.layer.borderWidth = 0.5
+                    view.addSubview(squareV)
+                    
+                    let key = "\(i)|\(j)"
+                    //cells[key] = squareV
+                    
+                    cells.updateValue(squareV, forKey: key)
+                    
+                    squareV.anchor(top:view.topAnchor, left: view.leftAnchor, bottom: nil,right: nil, paddingTop: CGFloat(j) * width, paddingLeft: CGFloat(i) * width, paddingBottom: 0, paddingRight: 0, width: width, height: width)
+                }
+            }
+           
+        } else {
+            
+            for j in 0...numViewPerColum{
+                for i in 0...numViewPerRow{
+                    let squareV = UIView()
+                    squareV.backgroundColor = randomColor()
+                    //squareV.frame = CGRect(x: CGFloat(i) * width, y:
+                    //  CGFloat(j) * width, width: width, height: width)
+                    squareV.layer.borderColor = UIColor.black.cgColor
+                    squareV.layer.borderWidth = 0.5
+                    view.addSubview(squareV)
+                    
+                    let key = "\(i)|\(j)"
+                    //cells[key] = squareV
+                    
+                    cells.updateValue(squareV, forKey: key)
+                    
+                    squareV.anchor(top:view.topAnchor, left: view.leftAnchor, bottom: nil,right: nil, paddingTop: CGFloat(j) * width, paddingLeft: CGFloat(i) * width, paddingBottom: 0, paddingRight: 0, width: width, height: width)
+                }
+            }
+            
+        }
+
+        
+       
+        
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture)))
+    }
+    
+    var selectedcellV: UIView?
+    func handlePanGesture(gesture: UIPanGestureRecognizer){
+        let loc:CGPoint = gesture.location(in: view)
+        
+        let i = Int(loc.x / width)
+        let j = Int(loc.y / width)
+        print(i,j)
+        
+        let key = "\(i)|\(j)"
+        guard let cellV = cells[key] else {return}
+       
+        if selectedcellV != cellV {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            
+            
+            self.selectedcellV?.layer.transform = CATransform3DIdentity
+        }, completion: nil)
+        
+        }
+        
+        selectedcellV = cellV
+        
+        
+        view.bringSubview(toFront: cellV)
+        
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            cellV.layer.transform = CATransform3DMakeScale(3, 3, 3)
+        },completion: nil)
+        
+        if gesture.state == .ended {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                
+                
+                self.selectedcellV?.layer.transform = CATransform3DIdentity
+            }, completion: nil)
+            
+        }
+            
+    
+        
+        
+        
+        
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        DispatchQueue.main.async() {
+            self.width = self.view.bounds.size.width
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    fileprivate func randomColor() -> UIColor {
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
-
 
 }
 
---
+++ gridAnimation/extension.swift
@@ -0,0 +1,67 @@
+//
+//  extension.swift
+//  gridAnimation
+//
+//  Created by eric yu on 5/2/17.
+//  Copyright © 2017 eric yu. All rights reserved.
+//
+import UIKit
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?,  paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+}
+
+
+
+class InsetLabel: UILabel {
+    let topInset = CGFloat(20)
+    let bottomInset = CGFloat(20)
+    let leftInset = CGFloat(0)
+    let rightInset = CGFloat(0)
+    
+    override func layoutSubviews() {
+        
+        //self.preferredMaxLayoutWidth = automatic
+        super.layoutSubviews()
+    }
+    
+    override func drawText(in rect: CGRect) {
+        let insets: UIEdgeInsets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: UIEdgeInsetsInsetRect(rect, insets))
+    }
+    
+    override public var intrinsicContentSize: CGSize {
+        var intrinsicSuperViewContentSize = super.intrinsicContentSize
+        intrinsicSuperViewContentSize.height += topInset + bottomInset
+        intrinsicSuperViewContentSize.width += leftInset + rightInset
+        return intrinsicSuperViewContentSize
+    }
+}
